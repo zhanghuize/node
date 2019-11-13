de181847826b6608c9664b812cb19e65805f0f34
zhangzejun <2216897283@qq.com>
2019-11-13 19:22:59 +0800

    这是分页

--- 校园超市/serverl/app/middleware/oauth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (opt, app) => {
-	return async function oauth(ctx, next) {
-		console.log('asd');
-		await next();
-		console.log('asd');
-		console.log(ctx.session.userinfo);
-	};
-};
--- 校园超市/serverl/app/service/user.js
+++
@@ -1,8 +0,0 @@
-const { Service } = require('egg');
-
-class UserService extends Service {
-	async login({ username, password }) {
-		return this.app.mysql.select('user1', { username, password });
-	}
-}
-module.exports = UserService;
